f743c5b02bdf913ecd02cbcea27e1f4ad6bd0ec7
Sunny <133858574+s-l-coder@users.noreply.github.com>
2023-07-07 22:15:35 +0800

    Add files via upload

--- Project-Crawling/Insert_Data_into_MySQL.py
+++ Project-Crawling/Insert_Data_into_MySQL.py
@@ -1,36 +1,71 @@
+######載入資料範例###############
+'''
+371250
+替代役役男死亡傷病及其家庭災害慰勞金補助
+passaway
+政府單位
+勞保局替代役役男於服役期間發生傷病住院，因公致傷病門診手術、傷病停役或家庭發生特別災害者，即可申請慰勞金。
+無
+https://www.gov.tw/News3_Content.aspx?n=2&s=371250
+'''
+#注意載入檔名!!!
+#####以上可刪除
+
 import pymysql
+import re
 
-
-#資料庫連線設定
+# 資料庫連線設定
 db = pymysql.connect(host='linebot-subsidy-mysql.fly.dev', port=3306, user='guest', passwd='theflymysql', db='Allowance_Master', charset='utf8')
-#建立操作游標
+# 建立操作游標
 cursor = db.cursor()
-#SQL語法
+# SQL語法
 
-sql = "INSERT INTO AllowanceDetails(serial_no, name, category, organization_name, url, content, condition_list) VALUES (%s, %s, %s, %s, %s, %s, %s);"
-f = open('爬蟲練習/subsidy_crawling_result_20230706.txt','r',encoding="utf-8")
-
+# 新增資料語法insertinto
+sql = "INSERT INTO AllowanceDetails(serial_no, name, category, organization_name, url, content, condition_list) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+# 開啟更新檔(記得修改檔案的相對路徑)
+f = open('subsidy_crawling_result_20230707.txt', 'r', encoding="utf-8")
+insert_data_num = 0
+update_data_num = 0
 while True:
     try:
-        serial_no = f.readline()
-        name = f.readline()
-        category = f.readline()
-        organization_name = f.readline()
-        title_1 = f.readline() #服務內容
-        content = f.readline()
-        title_2 = f.readline() #申辦資格
-        condition_list = f.readline()
-        url = f.readline()
-        new_data = (serial_no, name, category, organization_name, url, content, condition_list)
-        cursor.execute(sql,new_data)
-        db.commit()  #提交修改
-        print('success')
+        line = f.readline().strip()  #消除換行符號
+        if line == "":
+            break
+        else:
+            serial_no = line
+            name = f.readline().strip()
+            category = f.readline().strip()
+            organization_name = f.readline().strip()
+            content = f.readline().strip()
+            condition_list = f.readline().strip()
+            url_ = f.readline().strip()
+            new_data = (serial_no, name, category, organization_name, url_, content, condition_list)
+            cursor.execute(sql, new_data)  # 執行指令
+            db.commit()  # 提交至SQL指令
+            insert_data_num += 1
+            print(name,'success')
+            
     except EOFError:
         break
-    except Exception as e:  #發生錯誤時停止執行SQL
-        db.rollback()
-        print('error')
-        print(e)
 
-#關閉連線
+# 發生錯誤時停止執行SQL
+    except Exception as e:
+        db.rollback()
+        if re.search(r'Duplicate entry', str(e)):
+            update_subsidy = "UPDATE AllowanceDetails SET serial_no = %s, category = %s, organization_name = %s, url = %s, content = %s, condition_list = %s WHERE name = %s"
+            values = (serial_no, category, organization_name, url_, content, condition_list, name)
+            cursor.execute(update_subsidy, values)
+            db.commit()
+            update_data_num += 1
+            print(name,'update')
+            continue
+        else:
+            print(name,'error')
+            print(e)
+            break
+
+print("插入資料共",insert_data_num,"筆")
+print("更新資料共",update_data_num,"筆")
+# 關閉連線,關閉檔案
+f.close()
 db.close()
